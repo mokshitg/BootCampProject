023f5b72fbf2421affb5a7fa466c28c86cea11e0
mokshitg <155443283+mokshitg@users.noreply.github.com>
2024-01-18 20:45:30 +0530

    Add files via upload

---
+++ src/main/java/com/Stocks/BootCampProject/entity/User.java
@@ -0,0 +1,24 @@
+package com.Stocks.BootCampProject.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int userId;
+
+    String userName;
+
+}
